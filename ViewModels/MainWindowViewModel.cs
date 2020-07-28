ac8e0a4b503254e827f095bbbadb827f7ba33a3a
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2020-07-28 08:01:03 -0500

    Fixed Issue

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+using System.Reactive;
+using System.Collections.ObjectModel;
+using ConwayCore;
+using Avalonia.Controls;
+
+namespace ProjectConwayUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> Items { get; }
+
+        public ReactiveCommand<Unit, Unit> AddItemsCmd { get; }
+
+        public Image BGImage { get; }// background image of each tile
+        
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<string>(GenerateItems(1));
+            AddItemsCmd = ReactiveCommand.Create(() => PushItem("This is a test item to showcase the ConwayCore -> ConwayUI_Avalonia bridge Hello from the console realm :)"));
+        }
+
+        public void AddItems(int count = 10)
+        {
+            GenerateItems(count).ForEach(item => Items.Add(item));
+            Items.Add("Last Item from this Addition");
+        }
+        public void PushItem(string item)//seperate alias for ConwayCore link
+        {
+            Items.Add(item);
+
+        }
+
+        private List<string> GenerateItems(int count)
+        {
+            var list = new List<string>(count);
+            for (int i = 0; i < count; i++)
+            {
+                list.Add($"New String @{++Counter}");
+            }
+
+            return list;
+        }
+
+        private int Counter { get; set; }
+    }
+}
---
+++ Views/ListView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ProjectConwayUI.Views
+{
+    public class ListView : UserControl
+    {
+        public ListView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
